7cf5066bee649ebc56aeb13de5bd5bbfb7a6fc7d
psjin0329 <psjinsub0329@gmail.com>
2024-11-26 13:43:46 +0900

    高速化 Fixes #7

--- S2MFD/tools.py
+++ S2MFD/tools.py
@@ -4,9 +4,17 @@ Tools for S2MFD calculations
 '''
 import sys
 import numpy as np
-from numba import njit
+from numba import njit, float64
+from numba.types import Array, Tuple, unicode_type
 
-@njit
+#TODO: completed
+@njit(Array(float64, 2, 'C', False, aligned=True)
+      (
+      Array(float64, 2, 'C', False, aligned=True), 
+      Array(float64, 0, 'C', False, aligned=True), 
+      unicode_type  
+      ), 
+      cache=True)
 def drr1(qq,drr,dir):
     '''
     To calculate 1st order accuracy derivative in r direction
@@ -27,13 +35,28 @@ def drr1(qq,drr,dir):
         i0, i1 = 0, qq.shape[0]-1
     else:
         print('Error: dir must be up or dw')
-   
+
+    # vectorization
+    # dqq = np.zeros_like(qq)
+    # dqq[i0:i1,:] = (qq[1:,:] - qq[:-1,:])/drr
+
+    # for loop
+    inum = qq.shape[0]
+    jnum = qq.shape[1]
     dqq = np.zeros_like(qq)
-    dqq[i0:i1,:] = (qq[1:,:] - qq[:-1,:])/drr
+    for i in range(i1-i0):
+        for j in range(jnum):
+            dqq[i+i0,j] = (qq[i+1,j] - qq[i,j])/drr
    
     return dqq
 
-@njit
+#TODO: completed
+@njit(Array(float64, 2, 'C', False, aligned=True)
+      (
+      Array(float64, 2, 'C', False, aligned=True), 
+      Array(float64, 0, 'C', False, aligned=True), 
+      ), 
+      cache=True)
 def drr2(qq,drr):
     '''
     To calculate 2nd order accuracy derivative in r direction
@@ -44,12 +67,29 @@ def drr2(qq,drr):
     Output:
         dqq: differentiated quantity (numpy 2D array)
     '''   
+
+    # vectorization
+    # dqq = np.zeros_like(qq)
+    # dqq[1:-1,:] = (qq[2:qq.shape[0],:] - qq[0:-2,:])/drr*0.5
+
+    # for loop
+    inum = qq.shape[0]
+    jnum = qq.shape[1]
     dqq = np.zeros_like(qq)
-    dqq[1:-1,:] = (qq[2:qq.shape[0],:] - qq[0:-2,:])/drr*0.5
+    for i in range(inum-2):
+        for j in range(jnum):
+            dqq[i+1,j] = (qq[i+2,j] - qq[i,j])/drr*0.5
    
     return dqq
-   
-@njit   
+
+#TODO: completed
+@njit(Array(float64, 2, 'C', False, aligned=True)
+      (
+      Array(float64, 2, 'C', False, aligned=True), 
+      Array(float64, 0, 'C', False, aligned=True), 
+      unicode_type  
+      ), 
+      cache=True) 
 def dth1(qq,dth,dir):
     '''
     To calculate 1st order accuracy derivative in theta direction
@@ -71,12 +111,26 @@ def dth1(qq,dth,dir):
     else:
         print('Error: dir must be up or dw')
    
+    # vectorization
+    # dqq = np.zeros_like(qq)
+    # dqq[:,j0:j1] = (qq[:,1:] - qq[:,:-1])/dth
+
+    # for loop
+    inum = qq.shape[0]
     dqq = np.zeros_like(qq)
-    dqq[:,j0:j1] = (qq[:,1:] - qq[:,:-1])/dth
-   
+    for i in range(inum):
+        for j in range(j1-j0):
+            dqq[i,j+j0] = (qq[i,j+1] - qq[i,j])/dth
+        
     return dqq
 
-@njit
+#TODO: completed
+@njit(Array(float64, 2, 'C', False, aligned=True)
+      (
+      Array(float64, 2, 'C', False, aligned=True),  
+      Array(float64, 0, 'C', False, aligned=True)    
+      ), 
+      cache=True)
 def dth2(qq,dth):
     '''
     To calculate 2nd order accuracy derivative in theta direction
@@ -87,10 +141,19 @@ def dth2(qq,dth):
     Output:
         dqq: differentiated quantity (numpy 2D array)
     '''
-   
+    
+    # vectorization
+    # dqq = np.zeros_like(qq)
+    # dqq[:,1:-1] = (qq[:,2:qq.shape[1]] - qq[:,0:-2])/dth*0.5
+
+    # for loop
+    inum = qq.shape[0]
+    jnum = qq.shape[1]
     dqq = np.zeros_like(qq)
-    dqq[:,1:-1] = (qq[:,2:qq.shape[1]] - qq[:,0:-2])/dth*0.5
-   
+    for i in range(inum):
+        for j in range(jnum-2):
+            dqq[i,j+1] = (qq[i,j+2] - qq[i,j])/dth*0.5
+
     return dqq
 
 #########

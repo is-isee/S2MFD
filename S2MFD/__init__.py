e705a715b01f8556eedc8e3786ba527013f8572a
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2024-12-06 06:34:33 +0900

    S2MFDのドキュメンテーションを追加し、パッケージの使用例を示す

--- S2MFD/__init__.py
+++ S2MFD/__init__.py
@@ -1,3 +1,14 @@
+"""
+S2MFD: A Python package for solving 2D mean field dynamo equations in background of solar/stellar convection zone.
+
+Example
+-------
+>>> import S2MFD
+>>> S2MFD.run_simulation()
+
+
+"""
+
 paramdir = 'parameters/'
 
 from .main_functions import *
@@ -7,6 +18,8 @@ from .setup import Setup
 from .data import Data
 from .time_marching import *
 from .boundary_condition import *
+
+__all__ = ['Cfg', 'Grid', 'Seetup', 'Data']
 
 try:
     from ._version import version as __version__

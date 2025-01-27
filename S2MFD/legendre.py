e54a1fca342a6a386074f6255f3ddf08127c943e
Yuya-Shimizu-NagoyaU <strkgundam.gatx105@gmail.com>
2025-01-27 14:03:22 +0900

    ナイキスト波数に合わせてlmaxを設定

--- S2MFD/legendre.py
+++ S2MFD/legendre.py
@@ -20,7 +20,7 @@ class Legendre:
     def __init__(self,grid):
         # 0<θ<πで定義
         self.costh = np.cos(grid.th[grid.margin:grid.jxg-grid.margin])
-        self.lmax = grid.jx//2
+        self.lmax = grid.jx
         self.P1n = np.zeros((self.lmax,grid.jx))
         # n = 1
         self.P1n[1,:] = -(1-self.costh**2)**0.5

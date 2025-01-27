9ae2dca243477d4c1b6d0d74ac9609d3401ddaef
Yuya-Shimizu-NagoyaU <strkgundam.gatx105@gmail.com>
2025-01-27 16:36:40 +0900

    θとnを改善

--- S2MFD/physics/boundary_condition.py
+++ S2MFD/physics/boundary_condition.py
@@ -59,34 +59,34 @@ def boundary_condition(Bph, Aph, cfg, grid, legendre):
          P1Sum += (n+1)*ant[n]/cfg.RSUN*legendre.P1n[n]
       """
       
-      # TODO 実際の計算には1~63のみ使用する
       # 必要な配列を初期化
-      # TODO termnum→lmax
       ant = np.zeros(legendre.lmax-1) # (63,)
       sinth = np.sin(grid.th[grid.margin:grid.jxg - grid.margin])  # θのサイン値(128,)
       sinth_ex = np.repeat(sinth[:, np.newaxis], legendre.lmax-1, axis=1)  # (128, 63)
       P1Sum = np.zeros_like(sinth)  # 合計用配列(128,)
-
+      RSUN = cfg.RSUN/cfg.RSUN
       # Aph, legendre.P1n の一部を事前に切り出し
       Aph_reduced = Aph[grid.ixg - 2, 0:grid.jx]  # Aph の固定行部分(128,)
       # n(0~63まで入っている)
-      P1n_reduced = legendre.P1n[1:, :]    # Legendre多項式部分(63,128)
+      P1n_reduced = legendre.P1n[1:, :].T   # Legendre多項式部分(n=1~127)
       Aph_ex = np.repeat(Aph_reduced[:, np.newaxis], legendre.lmax-1, axis=1)  # (128, 63)
       # a_n(t) の計算をベクトル化
       # n = 1 から始まるため、スライスで範囲を調整
       n_values = np.arange(1, legendre.lmax)  # n のインデックスを作成(1~63)
-      coefficients = (2 * n_values + 1) / (2 * n_values * (n_values + 1))  # 係数部分(63この係数、1~63に対応する)
+      coefficients = (2 * n_values + 1) / (2 * n_values * (n_values + 1))*RSUN**(n_values+1)  # 係数部分(63この係数、1~63に対応する)
 
       # Equation 34: ベクトル化された積分計算(nこ出てきてほしい)
-      itg = np.sum(Aph_ex * P1n_reduced[n_values-1, :].T * sinth_ex * grid.dth, axis=0)
+      itg = np.sum(Aph_ex * P1n_reduced * sinth_ex * grid.dth, axis=0)
       # ant の計算 (ベクトル化済み)
-      ant[n_values-1] = coefficients * itg
+      ant = coefficients * itg
+      # θを追加
       ant_ex = np.repeat(ant[:, np.newaxis], grid.jx, axis=1) 
-      n_values_ex = np.repeat(ant[:, np.newaxis], grid.jx, axis=1)
+      # θを追加
+      n_values_ex = np.repeat(n_values[:, np.newaxis], grid.jx, axis=1)
       # print(ant.shape)
       # Equation 35: ベクトル化された P1Sum の更新(n=1~63までの足し算をしたい)
       P1Sum = np.sum(
-         ((n_values_ex[n_values-1,:] + 1) * ant_ex[n_values-1,:] / cfg.RSUN * P1n_reduced[n_values-1, :]),
+         ((n_values_ex + 1) * ant_ex / RSUN**(n_values_ex + 2) * P1n_reduced.T),
          axis=0
       )
       
@@ -113,49 +113,3 @@ def boundary_condition(Bph, Aph, cfg, grid, legendre):
       Bph[grid.margin:grid.ixg-grid.margin,grid.jxg-j-1] = -Bph[grid.margin:grid.ixg-grid.margin,grid.jxg-2*grid.margin+j]
       Aph[grid.margin:grid.ixg-grid.margin,grid.jxg-j-1] = -Aph[grid.margin:grid.ixg-grid.margin,grid.jxg-2*grid.margin+j]
    return Bph, Aph
-
-# # associated legendre polynomials (P^1_n(cosθ))
-# def calculate_associated_legendre_polynomials(xx):
-#    # associated legendre polynomials (P^1_n(cosθ))
-#    # Final term number
-#    # grid数で変化するので調べる。
-#    termnum = num
-#    # n = 0, 1
-#    lelist      = np.zeros((len(xx), termnum+1)) 
-#    lelist[:,1] = -(1-xx**2)**0.5
-#    # n = 2, 3, ..., termnum
-#    for l in range(2, termnum+1):
-#       # recurrence relation
-#       lelist[:,l] = ((2*l-1)/(l-1))*xx*lelist[:,l-1]-(l/(l-1))*lelist[:,l-2]
-#    return lelist
-
-# Dikpati+1999
-# def top_boundary_condition(Bph, Aph, grid, legendre):
-#    PnS = np.zeros_like(grid.TH)
-#    ale = np.zeros(legendre.termnum)
-#    itg = np.zeros_like(grid.th)
-
-#    # a_n(t)
-#    # n are odd numbers to maintain antisymmetry(north⇔south)
-#    for i in range(0, legendre.termnum):
-#       # integrate (range:0~π)
-#       for j in range(grid.margin, grid.jxg - grid.margin):
-#          itg[i] += Aph[grid.ixg-grid.margin-1,j]*legendre.P1n[i,j]*np.sin(grid.th[j])* grid.dth
-#       # a_n(t)
-#       ale[i] = (2*i+1)*cfg.RSUN**(i+1)/(2*i*(i+1))*itg[i]
-            
-#    # a_n(t)を用いて、(35)式右辺をもとめる
-#    for i in range(0, termnum):
-#       PnS += -(i+1)*ale[i]/cfg.RSUN**(i+2)*legendre.P1n[i,:]
-      
-#    # top boundary condition
-#    # no electrical current
-#    # Bph = 0, smoothly match Aph with an exterior potential field solution
-#    # TODO margin=1にのみ対応
-#    for i in range(0, grid.margin):
-#       Bph[grid.ixg-i-1,grid.margin:grid.jxg-grid.margin] = -Bph[grid.ixg-2*grid.margin+i,grid.margin:grid.jxg-grid.margin]
-#       Aph[grid.ixg-i-1,grid.margin:grid.jxg-grid.margin] = +Aph[grid.ixg-i-2,grid.margin:grid.jxg-grid.margin] \
-#          -grid.drr*PnS
-#       # Aph[grid.ixg-i-1,grid.margin:grid.jxg-grid.margin] = +Aph[grid.ixg-2*grid.margin+i,grid.margin:grid.jxg-grid.margin] \
-#       #    -i*grid.drr*PnS
-#    return Bph, Aph

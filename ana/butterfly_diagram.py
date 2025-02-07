f2395b645001d90d13a525ec7a1a66e9427af197
Yuya-Shimizu-NagoyaU <strkgundam.gatx105@gmail.com>
2025-02-07 16:44:10 +0900

    蝶形図の描画用コード

--- ana/butterfly_diagram.py
+++ ana/butterfly_diagram.py
@@ -5,9 +5,9 @@ sys.path.append('../')
 import S2MFD
 
 # datadir = '../data_alpha_omega_etaconst/'
-# datadir = '../data_alpha_omega/'
+datadir = '../data_alpha_omega/'
 # datadir = '../data_flux_transport/'
-datadir = '../data_potential/'
+# datadir = '../data_potential/'
 data = S2MFD.Data.initial_load(datadir)
 
 cfg = data.cfg
@@ -31,6 +31,7 @@ Brrt = np.zeros((grid.ixg,grid.jxg,n1-n0))
 Btht = np.zeros((grid.ixg,grid.jxg,n1-n0))
 Bpht = np.zeros((grid.ixg,grid.jxg,n1-n0))
 for n  in range(n0,n1):
+    print(n)
     data.data_load(n)
     Brr, Bth = S2MFD.physics.poloidal_mag(data.Aph, grid.RR, grid.sinTH, grid.drr, grid.dth)
     d = np.load(file=datadir+'data.'+str(n).zfill(6)+'.npz')
@@ -40,6 +41,10 @@ for n  in range(n0,n1):
     Bpht[:,:,n-n0] = d['Bph']
     
 #
+Bpht_7 = Bpht[1+np.argmin(abs(grid.rr-0.7*cfg.RSUN)),:,:]
+Brrt_s = Brrt[-2,:,:]
+
+
 Bpht0 = Bpht[1+np.argmin(abs(grid.rr-0.7*cfg.RSUN)),np.argmin(abs(grid.th-30/180*np.pi)),:]
 Brrt0 = Brrt[-2,np.argmin(abs(grid.th-60/180*np.pi)),:]
 
@@ -48,31 +53,24 @@ Bpht0_sign_diff = np.diff(Bpht0_sign)
 
 ns = np.where(Bpht0_sign_diff == +2)[0][-2]
 ne = np.where(Bpht0_sign_diff == +2)[0][-1]
-
+# timeu = (timet[ns:ne]-timet[ns])/tau_diff
+timeu = (timet[ns:]-timet[ns])/tau_diff
+B_range = 3
 plt.clf()
 plt.close('all')
-fig = plt.figure('J08_test',figsize=(6,10))
+fig = plt.figure('Butterfly Diagram',figsize=(6,10))
 ax1 = fig.add_subplot(2,1,1)
 ax2 = fig.add_subplot(2,1,2)
+# Bpht0u = Bpht0[ns:ne]
+# Brrt0u = Brrt0[ns:ne]
+# nw = ne - ns
+# nm = np.argmax(Bpht0u)
+ax1.pcolormesh(timeu,grid.th/np.pi*180,Bpht_7[:,ns:],cmap='bwr',shading='auto')
+ax2.pcolormesh(timeu,grid.th/np.pi*180,Brrt_s[:,ns:],vmax=B_range*1e-2,vmin=-B_range*1e-2,cmap='bwr',shading='auto')
 
-timeu = (timet[ns:ne]-timet[ns])/tau_diff
-Bpht0u = Bpht0[ns:ne]
-Brrt0u = Brrt0[ns:ne]
-nw = ne - ns
-nm = np.argmax(Bpht0u)
-
-ax1.plot(timeu,Bpht0u)
-ax2.plot(timeu,Brrt0u)
-
-ax1.set_ylabel(r'$B_\phi$: $r=0.7R_\odot$, $\theta=30^\circ$')
-ax2.set_ylabel(r'$B_r$: $r=R_\odot$, $\theta=60^\circ$')
+ax1.set_ylabel(r'$B_\phi$: $r=0.7R_\odot$')
+ax2.set_ylabel(r'$B_r$: $r=R_\odot$')
 
 ax2.set_xlabel(r't/$\tau_\mathrm{diff}$')
 
 fig.tight_layout()
-
-print('Cycle time = ',timeu[-1])
-print('Max(Bph) =',np.max(Bpht0u))
-
-    
-    

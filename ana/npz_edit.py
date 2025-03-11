90d1bb41332590f2a1bfc2cbac490bc83937b2fd
Yuya-Shimizu-NagoyaU <strkgundam.gatx105@gmail.com>
2025-03-11 12:36:34 +0900

    特に変更なし

--- ana/npz_edit.py
+++ ana/npz_edit.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # .npz ファイルを開く
 datadir = "../data/"
-d = np.load(datadir + "data.000761.npz")
+d = np.load(datadir + "data.001165.npz")
 
 data = S2MFD.Data.initial_load(datadir)
 
@@ -32,4 +32,4 @@ Aph[base:,:S_equa] = gamma_s * Aph[base:,:S_equa]
 Aph[base:,N_equa:] = gamma_n * Aph[base:,N_equa:]
 
 # 修正後のデータを新しい .npz に保存
-np.savez(datadir + "data.000761.npz", Bph=Bph, Aph=Aph, time=time, n=n)
+np.savez(datadir + "data.001165.npz", Bph=Bph, Aph=Aph, time=time, n=n)

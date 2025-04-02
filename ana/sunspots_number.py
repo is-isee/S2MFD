b5e1466b5ee3fed0895c9987040941d4f9bd739a
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-04-02 15:17:41 +0900

    黒点とみなす範囲を絞った

--- ana/sunspots_number.py
+++ ana/sunspots_number.py
@@ -6,11 +6,12 @@ import S2MFD
 
 
 # 黒点数の計上
-# TODO 範囲はよく吟味
 N_num = np.zeros(n1)
 S_num = np.zeros(n1)
-thrsh = 3.5
+thrsh = 3.0
 base  = 1+np.argmin(abs(grid.rr-0.7*cfg.RSUN))
+stat  = 1+np.argmin(abs(grid.th- 40/180*np.pi))
+endd  = 1+np.argmin(abs(grid.th-140/180*np.pi))
 # グリッドに応じた北半球と南半球の分割
 if grid.jxg % 2==0: 
     S_equa = grid.jxg//2 - 1
@@ -19,13 +20,13 @@ else:
     S_equa = (grid.jxg-1)//2 - 1
     N_equa = S_equa + 2
 
-# TODO 範囲指定して黒点数を数えるようにしたい。
 # S_numとN_numは全く同じになる（南北対称だから当たり前）
 # n1は時間要素の最後の番号
 for t in range(n1):
-    S_num[t] = np.sum(Bpht[base,grid.margin:S_equa+1,t]<-thrsh) + np.sum(Bpht[base,grid.margin:S_equa+1,t]>thrsh)
-    N_num[t] = np.sum(Bpht[base,N_equa:grid.jxg-grid.margin,t]<-thrsh) + np.sum(Bpht[base,N_equa:grid.jxg-grid.margin,t]>thrsh)
-
+    # S_num[t] = np.sum(Bpht[base,grid.margin:S_equa+1,t]<-thrsh) + np.sum(Bpht[base,grid.margin:S_equa+1,t]>thrsh)
+    # N_num[t] = np.sum(Bpht[base,N_equa:grid.jxg-grid.margin,t]<-thrsh) + np.sum(Bpht[base,N_equa:grid.jxg-grid.margin,t]>thrsh)
+    S_num[t] = np.sum(Bpht[base,stat:S_equa+1,t]<-thrsh) + np.sum(Bpht[base,stat:S_equa+1,t]>thrsh)
+    N_num[t] = np.sum(Bpht[base,N_equa:endd,t]<-thrsh)   + np.sum(Bpht[base,N_equa:endd,t]>thrsh)
 # 観測に基づいた調整パラメタ
 kappa = 0.3
 S_num = kappa * S_num
@@ -40,6 +41,7 @@ N_num2 = np.convolve(N_num, conv_f, mode='same')#移動平均
 time_s = np.linspace(0,data.cfg.tend,data.cfg.tend//data.cfg.dtout)
 time_y = time_s/data.cfg.d2s/365
 plt.plot(time_y,S_num2,'r',label = 'sunspots number')
+# plt.xlim(200, 300)
 plt.xlabel('time(year)')
 plt.ylabel('sunspots number')
 plt.savefig("sunspots_number.png")

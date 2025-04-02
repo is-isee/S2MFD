77d285236dd0b959f3b4ef9f529901a9f8ca14d6
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-04-02 12:08:35 +0900

    Boulderで使えるように改良

--- ana/butterfly_diagram.py
+++ ana/butterfly_diagram.py
@@ -8,9 +8,10 @@ Brrt0 = Brrt[-2,np.argmin(abs(grid.th-60/180*np.pi)),:]
 Bpht0_sign = np.sign(Bpht0)
 Bpht0_sign_diff = np.diff(Bpht0_sign)
 
-ns = np.where(Bpht0_sign_diff == +2)[0][-3]
-ne = np.where(Bpht0_sign_diff == +2)[0][-1]
-
+# ns = np.where(Bpht0_sign_diff == +2)[0][-3]
+# ne = np.where(Bpht0_sign_diff == +2)[0][-1]
+ns = 0
+ne = 4999
 timeu = (timet[ns:ne]-timet[ns])/tau_diff
 time_year = (timet[ns:ne]-timet[ns])/86400/365
 B_range = 3
@@ -21,10 +22,10 @@ ax1 = fig.add_subplot(2,1,1)
 ax2 = fig.add_subplot(2,1,2)
 
 # butterfly diagram
-B_0 = 4.e4
+B_0 = 1.e5
 c1 = ax1.pcolormesh(time_year, grid.th/np.pi*180, B_0*Bpht_c[:,ns:ne], cmap='bwr', shading='auto')
-c2 = ax2.pcolormesh(time_year, grid.th/np.pi*180, B_0*Brrt_s[:,ns:ne], cmap='bwr', shading='auto')
-# c2 = ax2.pcolormesh(timeu,grid.th/np.pi*180,Brrt_s[:,ns:ne],vmax=B_range*1e-2,vmin=-B_range*1e-2,cmap='bwr',shading='auto')
+# c2 = ax2.pcolormesh(time_year, grid.th/np.pi*180, B_0*Brrt_s[:,ns:ne], cmap='bwr', shading='auto')
+c2 = ax2.pcolormesh(time_year,grid.th/np.pi*180,B_0*Brrt_s[:,ns:ne],vmax=B_range*1e3,vmin=-B_range*1e3,cmap='bwr',shading='auto')
 
 # カラーバーを追加
 fig.colorbar(c1, ax=ax1, orientation='vertical').set_label(r'$B_\phi$ (G)')
@@ -32,14 +33,14 @@ fig.colorbar(c2, ax=ax2, orientation='vertical').set_label(r'$B_r$ (G)')
 
 # sunspot
 Bpht_lim = Bpht_c[:,ns:ne]
-mask_p = Bpht_lim > 1.0
-mask_m = Bpht_lim < -1.0
-ax1.contourf(timeu, grid.th/np.pi*180, mask_p, levels=[0.5, 1.5], colors=['black'])
-ax1.contourf(timeu, grid.th/np.pi*180, mask_m, levels=[0.5, 1.5], colors=['black'])
+mask_p = Bpht_lim > 3.0
+mask_m = Bpht_lim < -3.0
+ax1.contourf(time_year, grid.th/np.pi*180, mask_p, levels=[0.5, 1.5], colors=['black'])
+ax1.contourf(time_year, grid.th/np.pi*180, mask_m, levels=[0.5, 1.5], colors=['black'])
 
 ax1.set_ylabel(r'$B_\phi$: $r=0.7R_\odot$')
 ax2.set_ylabel(r'$B_r$: $r=R_\odot$')
 
 ax2.set_xlabel('t(year)')
 
-fig.tight_layout()
+plt.savefig("butterfly.png")

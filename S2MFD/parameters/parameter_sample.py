ac59659e6cea64e40452ed12de439ab7f1bf15a7
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-04-07 15:58:03 +0900

    パラメタを少し変更

--- S2MFD/parameters/parameter_sample.py
+++ S2MFD/parameters/parameter_sample.py
@@ -13,7 +13,7 @@ def so0_time_dependent(time, ett, RSUN):
 def uu0_time_dependent(time, ett, RSUN):
     """時間 t に応じて uu0 を変化させる関数"""
     rey = 700  # 基本のスケール
-    return rey * ett / RSUN * (1 + np.sin(2 * np.pi * time / (1e10)))  # 例: コサイン波で変化
+    return rey * ett / RSUN * (1 + 0.5*np.sin(2 * np.pi * time / (5e9)))  # 例: コサイン波で変化
 
 # 時間依存の so0 と uu0 を計算
 # if 'simulation' not in globals():

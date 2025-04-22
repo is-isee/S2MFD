0dd643352d88d1ad9add3408f476bf33840d2e54
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-04-22 12:48:04 +0900

    cfg関連で変なことが起きたらここの影響である可能性が高い

--- S2MFD/bisection_main_functions.py
+++ S2MFD/bisection_main_functions.py
@@ -1,4 +1,4 @@
-sys.path.append('../')
+# sys.path.append('../')
 import S2MFD
 
 # ========================================================================================== #
@@ -7,17 +7,17 @@ def bisection_simulation(cfg=None, parameter_file=None, datadir=None, startpoint
    if datadir is None:
       print('You need to specify the datadir')
       return   
-   cfg, grid, n1, Bpht, Apht, uu0t, so0t, nt, ndt, timet = load_for_bisection(datadir)
-   Sunspot_N,Sunspot_N2 = snumbers_energy(cfg, grid, Bpht)
    if parameter_file is None:
       cfg = S2MFD.Cfg()
    else:
       cfg = S2MFD.Cfg(parameter_file)
    sim = S2MFD.Simulation(cfg)
+   n1, Bpht, Apht, uu0t, so0t, nt, ndt, timet = sim.load_for_bisection(datadir)
+   Sunspot_N,Sunspot_N2 = sim.snumbers_energy(Bpht)
    sim.initialize_simulation()
    sim.cfl_condition()
    sim.initial_for_bisection(Bpht, Apht, uu0t, so0t, nt, ndt, timet, startpoint)
-   sim.bisection_ver1(uu0t=uu0t,Bpht=Bpht,Apht=Apht)
+   sim.bisection_ver2(uu0t=uu0t,Bpht=Bpht,Apht=Apht)
    # sim.main_loop_for_bisection(Sunspot_N=Sunspot_N,uu0t=uu0t,Bpht=Bpht,Apht=Apht)
 # ========================================================================================== #
 

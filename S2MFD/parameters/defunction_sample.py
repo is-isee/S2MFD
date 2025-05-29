935293ad85eefe7908460cf9d1ac2a9adf82823c
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-05-29 22:17:04 +0900

    パラメタの値を設定

--- S2MFD/parameters/defunction_sample.py
+++ S2MFD/parameters/defunction_sample.py
@@ -6,31 +6,29 @@ boundary_condition_type = 'potential'
 rrmin = 0.60*RSUN
 
 
-"""
+
 # 正解
 def so0_time_dependent(time, ett, RSUN):
-    A = 10  # 基本のスケール
-    omega = 1/693792000  # 周期
+    A = 12  # 基本のスケール
+    omega = 1/680000000  # 周期
     B = 35  # 基本のスケール
-    C = np.pi/4
+    C = np.pi/5
     return (A * (np.sin((2 * np.pi * omega * time)-C)) + B) * ett / RSUN # 例: サイン波で変化
     # return cso * ett / RSUN
-"""
 
-"""
+
 # 正解
 def uu0_time_dependent(time, ett, RSUN):
     A = 70
     B = 700
-    C = np.pi/6
+    C = np.pi/1.5
     omega = 1/693782000
     return (A * (np.sin((2 * np.pi * omega * time)-C)) + B) * ett / RSUN # 例: サイン波で変化
     # return rey * ett / RSUN
-"""
 
-def so0_time_dependent(A,omega,B,C,time):
-    """時間 t に応じて so0 を変化させる関数"""
-    return (A * (np.sin((2 * np.pi * omega * time)-C)) + B) * ett / RSUN
+# def so0_time_dependent(A,omega,B,C,time):
+#     """時間 t に応じて so0 を変化させる関数"""
+#     return (A * (np.sin((2 * np.pi * omega * time)-C)) + B) * ett / RSUN
 
 # def uu0_time_dependent(A,omega,B,C,time):
 #     """時間 t に応じて uu0 を変化させる関数"""

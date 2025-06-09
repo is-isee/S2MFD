c1fae827690f378e233d6d4dca1236ada00a0a38
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-06-09 13:07:48 +0900

    フーリエ級数でs0u0を定義（完了）

--- S2MFD/parameters/fourierGA_sample.py
+++ S2MFD/parameters/fourierGA_sample.py
@@ -8,36 +8,41 @@ rrmin = 0.60*RSUN
 
 
 # 正解
+# 35付近
 def so0_time_dependent(time, ett, RSUN):
-    a0 = 0.0
-    a1 = 0.0
-    a2 = 0.0
-    a3 = 0.0
-    b1 = 0.0
-    b2 = 0.0
-    b3 = 0.0
-    omega = 1/693782000  # 1回転にかかる時間(秒) = 693782000秒
+    factor = 1.0
+    a0 = 35
+    a1 = 1*factor
+    a2 = 3*factor
+    a3 = 0.5*factor
+    b1 = 3*factor
+    b2 = 1.2*factor
+    b3 = 4*factor
+    
+    omega = 1/(693782000*4.3)
     """時間 t に応じて so0 を変化させる関数(フーリエ級数)"""
-    s0t = (a0*0.5   + a1*cos(2*pi*omega*time) + b1*sin(2*pi*omega*time)\
-                    + a2*cos(4*pi*omega*time) + b2*sin(4*pi*omega*time)\
-                    + a3*cos(6*pi*omega*time) + b3*sin(6*pi*omega*time)) * ett / RSUN
+    s0t = (a0   + a1*cos(2*pi*omega*time) + b1*sin(2*pi*omega*time)\
+                + a2*cos(4*pi*omega*time) + b2*sin(4*pi*omega*time)\
+                + a3*cos(6*pi*omega*time) + b3*sin(6*pi*omega*time)) * ett / RSUN
     return s0t
 
 
 # 正解
+# 700付近
 def uu0_time_dependent(time, ett, RSUN):
-    a0 = 0.0
-    a1 = 0.0
-    a2 = 0.0
-    a3 = 0.0
-    b1 = 0.0
-    b2 = 0.0
-    b3 = 0.0
-    omega = 1/693782000  # 1回転にかかる時間(秒) = 693782000秒
+    factor = 1.0
+    a0 = 600
+    a1 = 140*factor
+    a2 = 21*factor
+    a3 = 70*factor
+    b1 = 44*factor
+    b2 = 90*factor
+    b3 = 20*factor
+    omega = 1/(693782000*6)  # 1回転にかかる時間(秒) = 693782000秒
     """時間 t に応じて so0 を変化させる関数(フーリエ級数)"""
-    u0t = (a0*0.5   + a1*cos(2*pi*omega*time) + b1*sin(2*pi*omega*time)\
-                    + a2*cos(4*pi*omega*time) + b2*sin(4*pi*omega*time)\
-                    + a3*cos(6*pi*omega*time) + b3*sin(6*pi*omega*time)) * ett / RSUN
+    u0t = (a0   + a1*cos(2*pi*omega*time) + b1*sin(2*pi*omega*time)\
+                + a2*cos(4*pi*omega*time) + b2*sin(4*pi*omega*time)\
+                + a3*cos(6*pi*omega*time) + b3*sin(6*pi*omega*time)) * ett / RSUN
     return u0t
 
 

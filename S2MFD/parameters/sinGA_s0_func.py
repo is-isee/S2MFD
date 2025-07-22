885d12edbde6aab164eee71f74a320454050534d
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-07-22 15:39:07 +0900

    式の定義を修正、以降直接当てに行くものとする

--- S2MFD/parameters/sinGA_s0_func.py
+++ S2MFD/parameters/sinGA_s0_func.py
@@ -5,5 +5,5 @@ tend = 91000*d2s
 boundary_condition_type = 'potential'
 
 def so0_time_dependent(a0_s,a1_s,a2_s,omega_s,time):
-    s0t = (a0_s + a1_s*np.sin(1.0*omega_s*time) + a2_s*np.sin(2.0*omega_s*time)) * ett / RSUN
+    s0t = a0_s + a1_s*np.sin(1.0*omega_s*time) + a2_s*np.sin(2.0*omega_s*time)
     return s0t

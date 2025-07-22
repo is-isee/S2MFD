455515949d1ef86c0a8a7dfe741fbff042c0b1fa
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-07-22 14:00:01 +0900

    新たな正解データを準備

---
+++ S2MFD/parameters/GA_s0u0_true.py
@@ -0,0 +1,43 @@
+from S2MFD.parameters.defaults import *
+
+tend = 91000*d2s
+# boundary condition
+boundary_condition_type = 'potential'
+datadir = 'data_fousamp_s0u0/'
+
+
+# # 正解
+# # 35付近
+def so0_time_dependent(time, ett, RSUN):
+    factor = 1.0
+    a0 = 35
+    a1 = 1  *factor
+    a2 = 3  *factor
+    a3 = 0.5*factor
+    b1 = 3  *factor
+    b2 = 1.2*factor
+    b3 = 4  *factor
+    
+    omega = 2*np.pi/(21*365*60*60*24)  # 1回転にかかる時間(秒) = 21年
+    """時間 t に応じて so0 を変化させる関数(フーリエ級数)"""
+    s0t = (a0   + a1*np.cos(1.0*omega*time) + b1*np.sin(1.0*omega*time)\
+                + a2*np.cos(2.0*omega*time) + b2*np.sin(2.0*omega*time)\
+                + a3*np.cos(3.0*omega*time) + b3*np.sin(3.0*omega*time)) * ett / RSUN
+    return s0t
+
+def uu0_time_dependent(time, ett, RSUN):
+    factor = 1.0
+    a0 = 600
+    a1 = 140*factor
+    a2 = 21 *factor
+    a3 = 70 *factor
+    b1 = 44 *factor
+    b2 = 90 *factor
+    b3 = 20 *factor
+
+    omega = 2*np.pi/(32*365*60*60*24)  # 1回転にかかる時間(秒) = 32年
+    """時間 t に応じて so0 を変化させる関数(フーリエ級数)"""
+    u0t = (a0   + a1*np.cos(1.0*omega*time) + b1*np.sin(1.0*omega*time)\
+                + a2*np.cos(2.0*omega*time) + b2*np.sin(2.0*omega*time)\
+                + a3*np.cos(3.0*omega*time) + b3*np.sin(3.0*omega*time)) * ett / RSUN
+    return u0t

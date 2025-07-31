297f99dce2eacada974ee74b391e0b061539e60f
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-07-31 12:14:21 +0900

    観測に近いデータの生成に使った

--- S2MFD/parameters/parameter_sample.py
+++ S2MFD/parameters/parameter_sample.py
@@ -3,24 +3,6 @@ from S2MFD.parameters.defaults import *
 tend = 91000*d2s
 # boundary condition
 boundary_condition_type = 'potential'
-"""
-def so0_time_dependent(time, ett, RSUN):
-    cso = 35  # 基本のスケール
-    return cso * ett / RSUN * (1 + np.sin(2 * np.pi * time / (1e10)))  # 例: サイン波で変化
-    # return cso * ett / RSUN
 
-
-def uu0_time_dependent(time, ett, RSUN):
-    rey = 700  # 基本のスケール
-    # return rey * ett / RSUN * (1 + 0.5*np.sin(2 * np.pi * time / (5e9)))  # 例: コサイン波で変化
-    return rey * ett / RSUN
-"""
-# 時間依存の so0 と uu0 を計算
-# if 'simulation' not in globals():
-#     simulation = None
-    
-# if simulation == None:
-#     simulation = type('Simulation', (object,), {})()  # ダミーのオブジェクトを作成
-#     simulation.time = 0  # 初期時間を設定
-# so0 = so0_time_dependent(simulation.time, ett, RSUN)
-# uu0 = uu0_time_dependent(simulation.time, ett, RSUN)
+uu0=694.8876129746217 # so0=50における推定値、Jouveの値はso0=50.28735632183908なのでおおよそ同じと推測。
+datadir = 'data_nearOBS/'

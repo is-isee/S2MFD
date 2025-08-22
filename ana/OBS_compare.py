e08d4a68affe2b5133bfa4b3299899b93a8843d6
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-08-22 11:16:22 +0900

    ラベル調節

--- ana/OBS_compare.py
+++ ana/OBS_compare.py
@@ -87,52 +87,71 @@ so0t2= so0t
 uu0t2 = uu0t
 time2 = timet/data.cfg.d2s/365
 # ============================================================================== #
+# 基本ラベルサイズの指定
+size = 10
+
+# 日本語使用可能
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'IPAPGothic'
+# ============================================================================== #
+# 黒点数の比較グラフ
 # グラフの描画
 plt.figure(figsize=(10, 6))  # グラフのサイズを調整
-plt.plot(time1, SN1, 'r--', label='Observation',linewidth=2.5)  # ラベル名を明確に
-plt.plot(time2, SN2, 'b', label='GA inference')  # ラベル名を明確に
-# 軸ラベル
-plt.xlabel('Years', fontsize=20)
-plt.ylabel('Sunspots Number', fontsize=20)
-# 軸のメモリを細かく設定
-plt.xticks(fontsize=14)  # x軸の数値サイズを調整
-plt.yticks(fontsize=14)  # y軸の数値サイズを調整
+plt.plot(time1, SN1, 'r--', label='観測',linewidth=2.5)
+plt.plot(time2, SN2, 'b', label='推定')
+
+# 軸ラベル・タイトル
+plt.title('黒点数時間変化', fontsize=4*size)  # タイトルを追加
+plt.xlabel('年', fontsize=3*size)
+plt.ylabel('黒点相対数', fontsize=3*size)
+
+# 軸のメモリフォントサイズ
+plt.xticks(fontsize=2*size)  # x軸の数値サイズを調整
+plt.yticks(fontsize=2*size)  # y軸の数値サイズを調整
+
 # グリッドを追加して見やすく
 plt.grid(True, linestyle='--', alpha=0.7)
 # 凡例を表示
-plt.legend(fontsize=14, loc='upper right')  # 凡例を右上に固定
+plt.legend(fontsize=1.6*size, loc='upper right')  # 凡例を右上に固定
+plt.tight_layout()
 # グラフを保存
 plt.savefig("P_sunspots_number_compare.png", dpi=300)  # 解像度を高める
 plt.clf()
 
-plt.plot(time2,uu0t2,'b',label='GA inference')
-plt.xlabel('Years',fontsize=20)
-plt.ylabel(r'$u_0(\rm{cm/s})$',fontsize=20)
+# ============================================================================== #
+# u0の比較グラフ
+plt.plot(time2,uu0t2,'b',label='推定')
+plt.xlabel('年',fontsize=3*size)
+plt.ylabel(r'$u_0(\rm{cm/s})$',fontsize=3*size)
+plt.title(r'$u_0$ 時間変化', fontsize=4*size)  # タイトルを追加
 ymax = max(np.max(uu0t2), np.max(uu0t2)) * 1.1
 plt.ylim(bottom=0, top=ymax)
 # 軸のメモリを細かく設定
-plt.xticks(fontsize=14)  # x軸の数値サイズを調整
-plt.yticks(fontsize=14)  # y軸の数値サイズを調整
+plt.xticks(fontsize=2*size)  # x軸の数値サイズを調整
+plt.yticks(fontsize=2*size)  # y軸の数値サイズを調整
 # グリッドを追加して見やすく
 plt.grid(True, linestyle='--', alpha=0.7)
 # 凡例を表示
-plt.legend(fontsize=14, loc='upper right')  # 凡例を右上に固定
+plt.legend(fontsize=1.6*size, loc='upper right')  # 凡例を右上に固定
 # グラフを保存
 plt.savefig("P_u0_compare.png", dpi=300)
 plt.clf()
 
-plt.plot(time2,so0t2,'b',label='GA inference')
+# ============================================================================== #
+# so0の比較グラフ
+plt.plot(time2,so0t2,'b',label='推定')
 ymax = max(np.max(so0t2), np.max(so0t2)) * 1.1
 plt.ylim(bottom=0, top=ymax)
-plt.xlabel('Years',fontsize=20)
-plt.ylabel(r'$s_0(\rm{cm/s})$',fontsize=20)
+plt.xlabel('年',fontsize=3*size)
+plt.ylabel(r'$s_0(\rm{cm/s})$',fontsize=3*size)
+plt.title(r'$s_0$ 時間変化', fontsize=4*size)  # タイトルを追加
 # 軸のメモリを細かく設定
-plt.xticks(fontsize=14)  # x軸の数値サイズを調整
-plt.yticks(fontsize=14)  # y軸の数値サイズを調整
+plt.xticks(fontsize=2*size)  # x軸の数値サイズを調整
+plt.yticks(fontsize=2*size)  # y軸の数値サイズを調整
 # グリッドを追加して見やすく
 plt.grid(True, linestyle='--', alpha=0.7)
 # 凡例を表示
-plt.legend(fontsize=14, loc='upper right')  # 凡例を右上に固定
+plt.legend(fontsize=1.6*size, loc='upper right')  # 凡例を右上に固定
 # グラフを保存
 plt.savefig("P_s0_compare.png", dpi=300)
 plt.clf()

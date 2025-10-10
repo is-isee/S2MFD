5c7b56f8fe0084deecf9612146a98b112cfa011a
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-10-10 10:51:39 +0900

    厳密な推定区間にした

--- S2MFD/parameters/210_685.py
+++ S2MFD/parameters/210_685.py
@@ -6,10 +6,12 @@ boundary_condition_type = 'potential'
 
 def uu0_time_dependent(as_u,ae_u,a1_u,a2_u,a4_u,time):
     # 最適化区間の2倍で定義
-    inf_year = 52
+    inf_year = 52.05479452054692
     omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+    T_s = 0.0
+    T_e = inf_year*365*60*60*24
     # 線形関数
-    lin  = (as_u*(time[-1]-time)+ae_u*(time-time[0]))/(time[-1]-time[0])
+    lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
     u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
     return u0t
 # TODO s0の推定関数を書くこと

68b56306a156694846655e0fb122cdeed446f366
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-10-15 11:02:12 +0900

    名前を変更し、s0推定用に書き換えた

---
+++ S2MFD/parameters/sinGA_func.py
@@ -0,0 +1,43 @@
+from S2MFD.parameters.defaults import *
+
+tend = 91000*d2s
+# boundary condition
+boundary_condition_type = 'potential'
+
+# def uu0_time_dependent(as_u,ae_u,a1_u,a2_u,a4_u,time):
+#     # 最適化区間の2倍で定義
+#     inf_year = 53.809153043842755
+#     omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+#     T_s = 0.0
+#     T_e = inf_year*365*60*60*24
+#     # 線形関数
+#     lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
+#     u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
+#     return u0t
+
+def so0_time_dependent(as_s,ae_s,a1_s,a2_s,a4_s,time):
+    # 最適化区間の2倍で定義
+    inf_year = 53.809153043842755
+    omega_s = 2*np.pi/(inf_year*365*60*60*24*2)
+    T_s = 0.0
+    T_e = inf_year*365*60*60*24
+    # 線形関数
+    lin  = (as_s*(T_e-time)+ae_s*(time-T_s))/(T_e-T_s)
+    so0t = lin + a1_s*np.sin(1.0*omega_s*time) + a2_s*np.sin(2.0*omega_s*time) + a4_s*np.sin(4.0*omega_s*time)
+    return so0t
+
+def uu0_known(time):
+    inf_year = 53.809153043842755
+    omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+    as_u= 644.757697
+    ae_u= 704.818478
+    a1_u= 40.271709
+    a2_u= 56.997941
+    a4_u= 29.805212
+    T_s = 0.0
+    T_e = inf_year*365*60*60*24
+    lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
+    u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
+
+    return u0t 
+uu0_const = 644.757697

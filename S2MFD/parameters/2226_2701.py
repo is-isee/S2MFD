c4258c6c17697c211648f3143948c83a5d216fd2
Yuya-Shimizu-NagoyaU <yuyasmz1109.nagoya@outlook.jp>
2025-10-22 10:52:15 +0900

    推定のための変更

--- S2MFD/parameters/2226_2701.py
+++ S2MFD/parameters/2226_2701.py
@@ -4,40 +4,40 @@ tend = 91000*d2s
 # boundary condition
 boundary_condition_type = 'potential'
 
-def uu0_time_dependent(as_u,ae_u,a1_u,a2_u,a4_u,time):
-    # 最適化区間の2倍で定義
-    inf_year = 52.054794520545784
-    omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
-    T_s = 0.0
-    T_e = inf_year*365*60*60*24
-    # 線形関数
-    lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
-    u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
-    return u0t
-
-# def so0_time_dependent(as_s,ae_s,a1_s,a2_s,a4_s,time):
+# def uu0_time_dependent(as_u,ae_u,a1_u,a2_u,a4_u,time):
 #     # 最適化区間の2倍で定義
 #     inf_year = 52.054794520545784
-#     omega_s = 2*np.pi/(inf_year*365*60*60*24*2)
+#     omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
 #     T_s = 0.0
 #     T_e = inf_year*365*60*60*24
 #     # 線形関数
-#     lin  = (as_s*(T_e-time)+ae_s*(time-T_s))/(T_e-T_s)
-#     so0t = lin + a1_s*np.sin(1.0*omega_s*time) + a2_s*np.sin(2.0*omega_s*time) + a4_s*np.sin(4.0*omega_s*time)
-#     return so0t
-
-# def uu0_known(time):
-#     inf_year = 52.054794520545784
-#     omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
-#     as_u= 753.360841
-#     ae_u= 798.717979
-#     a1_u= -45.854816
-#     a2_u= 2.256662
-#     a4_u= -38.634266
-#     T_s = 0.0
-#     T_e = inf_year*365*60*60*24
 #     lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
 #     u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
+#     return u0t
 
-#     return u0t 
-# uu0_const = 753.360841
+def so0_time_dependent(as_s,ae_s,a1_s,a2_s,a4_s,time):
+    # 最適化区間の2倍で定義
+    inf_year = 52.054794520545784
+    omega_s = 2*np.pi/(inf_year*365*60*60*24*2)
+    T_s = 0.0
+    T_e = inf_year*365*60*60*24
+    # 線形関数
+    lin  = (as_s*(T_e-time)+ae_s*(time-T_s))/(T_e-T_s)
+    so0t = lin + a1_s*np.sin(1.0*omega_s*time) + a2_s*np.sin(2.0*omega_s*time) + a4_s*np.sin(4.0*omega_s*time)
+    return so0t
+
+def uu0_known(time):
+    inf_year = 52.054794520545784
+    omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+    as_u= 731.303719 
+    ae_u= 807.509283
+    a1_u= -40.363536 
+    a2_u= 50.981767 
+    a4_u= 44.633943
+    T_s = 0.0
+    T_e = inf_year*365*60*60*24
+    lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
+    u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
+
+    return u0t 
+uu0_const = 731.303719 
--- S2MFD/parameters/2701_2956.py
+++ S2MFD/parameters/2701_2956.py
@@ -4,25 +4,40 @@ tend = 91000*d2s
 # boundary condition
 boundary_condition_type = 'potential'
 
-def uu0_time_dependent(as_u,ae_u,a1_u,a2_u,a4_u,time):
+# def uu0_time_dependent(as_u,ae_u,a1_u,a2_u,a4_u,time):
+#     # 最適化区間の2倍で定義
+#     inf_year = 27.945205479451715
+#     omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+#     T_s = 0.0
+#     T_e = inf_year*365*60*60*24
+#     # 線形関数
+#     lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
+#     u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
+#     return u0t
+
+def so0_time_dependent(as_s,ae_s,a1_s,a2_s,a4_s,time):
     # 最適化区間の2倍で定義
     inf_year = 27.945205479451715
-    omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+    omega_s = 2*np.pi/(inf_year*365*60*60*24*2)
     T_s = 0.0
     T_e = inf_year*365*60*60*24
     # 線形関数
+    lin  = (as_s*(T_e-time)+ae_s*(time-T_s))/(T_e-T_s)
+    so0t = lin + a1_s*np.sin(1.0*omega_s*time) + a2_s*np.sin(2.0*omega_s*time) + a4_s*np.sin(4.0*omega_s*time)
+    return so0t
+
+def uu0_known(time):
+    inf_year = 27.945205479451715
+    omega_u = 2*np.pi/(inf_year*365*60*60*24*2)
+    as_u= 638.151878
+    ae_u= 539.739103
+    a1_u= -17.925003
+    a2_u= 71.258348 
+    a4_u= 157.136101
+    T_s = 0.0
+    T_e = inf_year*365*60*60*24
     lin  = (as_u*(T_e-time)+ae_u*(time-T_s))/(T_e-T_s)
     u0t = lin + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a4_u*np.sin(4.0*omega_u*time)
-    return u0t
-# TODO s0の推定関数を書くこと
-# TODO uu0_knownを書き直すこと
 
-# def uu0_known(time):
-#     omega_u = 2*np.pi/(28*365*60*60*24)
-#     a0_u=645.865306
-#     a1_u=-72.702397
-#     a2_u=122.933904
-#     a3_u=-15.331953
-#     u0t = a0_u + a1_u*np.sin(1.0*omega_u*time) + a2_u*np.sin(2.0*omega_u*time) + a3_u*np.sin(3.0*omega_u*time)
-#     return u0t 
-# uu0_const = 645.865306
+    return u0t 
+uu0_const = 638.151878
